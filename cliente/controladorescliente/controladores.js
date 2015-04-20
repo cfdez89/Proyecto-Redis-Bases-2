42fa92c3f1fcce9b17bb777ce6807533e0f056d0
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2015-04-19 19:49:49 -0600

    Se agrego una plantilla al frontend

--- cliente/controladorescliente/controladores.js
+++ cliente/controladorescliente/controladores.js
@@ -1,29 +1,3 @@
-'use strict';
-app.factory('socket', function ($rootScope) {
-  var socket = io.connect();
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    }
-  };
-});
-
-
 app.controller('ctlMensajes', ['$scope', '$http', 'socket', function ($scope, $http, socket){
 	$scope.mensajes = [];
 	
@@ -31,8 +5,8 @@ app.controller('ctlMensajes', ['$scope', '$http', 'socket', function ($scope, $h
     	console.log(message);
     });	
     socket.on('getUser', function(message){
-    	var res=[];
-    	for(var i=0; i<message.total.length; i++){
+    	var res = [];
+    	for(var i = 0; i < message.total.length; i++){
     		 $scope.mensajes.push(message.total[i]);
     	}
 
